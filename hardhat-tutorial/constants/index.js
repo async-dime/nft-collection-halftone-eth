9ea0c6c19e9d260e615a6adc74da66e0b5b10d05
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2022-02-06 21:18:27 +0700

    :zap: update contract metadata

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = '0x1552984Dbc4b3fc0e19D3317E8EC9501E4836A86';
 // URL to extract Metadata for a Halftone Eth NFT
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+const METADATA_URL = 'https://halftone-ethereum.vercel.app/api/';
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
